b187507b8a923f9477ad84928a89294e7cc5c97b
mekwalters <mekwalters@gmail.com>
2023-07-10 10:46:46 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const peopleAndAnimals = ['nuns', 'soccer players', 'dogs', 'ducks', 'lawyers', 'blondes', 'Americans', 'Canadians', 'Russians', 'Jedi', 'Wizards', 'zombies'];
+const questions = ['what would you like', 'who are you', 'where are you from', 'what do you want', 'is it hot out', ]
+const answers = ['water', 'milk', 'beer', 'whiskey', 'world domination', 'sushi', 'boba', 'its hotter than Hades', 'the Vatican', 'Hoth', 'Hogwarts', 'Darth Vader', 'respect', 'a pond', 'a bone', 'brains', 'flying carpets', 'lightsabers', 'world peace']
+console.log ('3 _________ walk into a bar'); //needs people & animals array
+console.log ('The bartender asks _________'); // needs questions array
+console.log ('They say ____________, of course.') // needs answers array
