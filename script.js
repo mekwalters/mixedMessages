c9a08960e9d8b6b08d2f8d0dc534babc5712e660
mekwalters <mekwalters@gmail.com>
2023-07-10 10:50:39 -0400

    Add explanatory comments

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//arrays to feed into the message generator
 const peopleAndAnimals = ['nuns', 'soccer players', 'dogs', 'ducks', 'lawyers', 'blondes', 'Americans', 'Canadians', 'Russians', 'Jedi', 'Wizards', 'zombies'];
 const questions = ['what would you like', 'who are you', 'where are you from', 'what do you want', 'is it hot out', ]
 const answers = ['water', 'milk', 'beer', 'whiskey', 'world domination', 'sushi', 'boba', 'its hotter than Hades', 'the Vatican', 'Hoth', 'Hogwarts', 'Darth Vader', 'respect', 'a pond', 'a bone', 'brains', 'flying carpets', 'lightsabers', 'world peace']
+
+//functions to randomize
+
+
+//output statements
 console.log ('3 _________ walk into a bar'); //needs people & animals array
 console.log ('The bartender asks _________'); // needs questions array
 console.log ('They say ____________, of course.') // needs answers array
